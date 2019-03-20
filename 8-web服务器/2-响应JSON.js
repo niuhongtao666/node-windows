e3c64b8df9ecb13a1f51e8cd4502cdc73cbee5ac
niuhongtao <18333602833@163.com>
2019-03-20 14:21:52 +0800

    8-web服务器/2-响应JSON.js

---
+++ 8-web服务器/2-响应JSON.js
@@ -0,0 +1,12 @@
+var http=require('http');
+http.createServer((req,res)=>{
+    res.writeHead(200,{'Content-Type':'application/json'});
+    var obj={
+        name:'zhangsan',
+        age:15
+    };
+    res.end(JSON.stringify(obj));
+    res.on('finish',function(){
+        console.log('finished')
+    });
+}).listen(8001);

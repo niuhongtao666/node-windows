0009348f025b1a4c9ad1cd309ca10618fc99dfbe
niuhongtao <18333602833@163.com>
2019-04-27 20:30:14 +0800

    17-上传图片

--- 17-上传图片/server.js
+++ 17-上传图片/server.js
@@ -1,8 +1,31 @@
 var express=require('express');
 var app=express();
+var fs=require('fs');
+var multer  = require('multer')
+// var upload = multer({ dest: 'uploads/' })
 var bodyParser=require('body-parser');
 var jsonType=bodyParser.json();
 var formType=bodyParser.urlencoded({extended:false});
+var createFolder = function(folder){
+    try{
+        fs.accessSync(folder); 
+    }catch(e){
+        fs.mkdirSync(folder);
+    }  
+};
+
+var uploadFolder = './uploadMore';
+createFolder(uploadFolder);
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, uploadFolder)
+    },
+    filename: function (req, file, cb) {
+        console.dir(file);
+      cb(null, file.originalname);
+    }
+  })
+var upload = multer({ storage: storage })
 app.post('/json',jsonType,(req,res)=>{
     console.log('请求体的内容是');
     console.dir(req.body);
@@ -10,6 +33,14 @@ app.post('/json',jsonType,(req,res)=>{
 app.post('/form',formType,(req,res)=>{
     console.log('请求体的内容是');
     console.dir(req.body);
+})
+app.get('/formFile',(req,res)=>{
+    console.log('成功读取表单html');
+    var formfile=fs.readFileSync('./formFile.html',{encoding:'utf-8'});
+    res.send(formfile);
+})
+app.post('/upload',upload.single('logo'),formType,(req,res)=>{
+    res.send('已经上传图片到文件夹');
 })
 app.listen(3003);
 console.log('服务启动');

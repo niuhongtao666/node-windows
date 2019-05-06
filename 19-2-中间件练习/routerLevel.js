3d84b4449a33cac12ebacd5306b7211df3d63c84
niuhongtao <18333602833@163.com>
2019-05-06 22:46:48 +0800

    19-梳理路由级中间件

--- 19-2-中间件练习/routerLevel.js
+++ 19-2-中间件练习/routerLevel.js
@@ -1,10 +1,10 @@
 var express=require('express');
-var app = express()
+var app = express();
 var router = express.Router();
-
+var router1 = express.Router();
 // predicate the router with a check and bail out when needed
 router.use(function (req, res, next) {
-  if (req.headers['x-auth']) return next('router')
+  if (!req.headers['x-auth']) return next('router')
   next()
 },function (req, res, next) {
     res.send('regular')
@@ -16,13 +16,13 @@ router.get('/', function (req, res) {
 })
 router.get('/home', function (req, res) {
     res.send('hello, user!home')
-  })
-  
+})
+    
   
 // use the router and 401 anything falling through
 app.use('/admin', router, function (req, res) {
 //   res.sendStatus(401)
-    res.send('haha')
+    res.send('haha1')
 })
 app.listen(3003);
 console.log('启动服务');

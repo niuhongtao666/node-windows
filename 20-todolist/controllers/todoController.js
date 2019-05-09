fb9ebca53e2aea4635e4dbbeee27b4e04e21d576
niuhongtao <18333602833@163.com>
2019-05-09 23:21:19 +0800

    todolist-实现功能

--- 20-todolist/controllers/todoController.js
+++ 20-todolist/controllers/todoController.js
@@ -1,12 +1,17 @@
+var data=[{item:'walking dogs'},{item:'playing with cats'},{item:'writing dinaries'}];
+var bodyparser=require('body-parser');
+var dataReceive=bodyparser.urlencoded({extended:false});
 module.exports=function(app){
     app.get('/todo',(req,res)=>{
         // res.send('get-todo');
-        res.render('todo');
+        res.render('todo',{todos:data});
     });
-    app.post('/todo',(req,res)=>{
-        res.send('post-todo');        
+    app.post('/todo',dataReceive,(req,res)=>{
+        data.push(req.body); 
+        res.json(data);
     });
-    app.delete('/todo',(req,res)=>{
-        res.send('delete-todo');                
+    app.delete('/todo/:item',(req,res)=>{
+        data=data.filter(every=>every.item.replace(/ /g,'-')!==req.params.item);
+        res.json(data);                
     });
 };

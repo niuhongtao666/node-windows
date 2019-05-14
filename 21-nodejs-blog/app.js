17d7bc8a35028a9f91da23f69e50a2e894cfa1da
niuhongtao <18333602833@163.com>
2019-05-14 18:23:45 +0800

    21-pug模版引擎

--- 21-nodejs-blog/app.js
+++ 21-nodejs-blog/app.js
@@ -1,7 +1,18 @@
 const express=require('express');
+const path=require('path');
 const app=express();
+app.set('views',path.join(__dirname,'views'));
+app.set('view engine','pug');
 app.get('/',(req,res)=>{
-    res.send('hello');
+    let articles=[
+        {id:1,title:'title1',author:'niu1'},
+        {id:2,title:'title2',author:'niu2'},
+        {id:3,title:'title3',author:'niu3'},
+    ];
+    res.render('index',{articles:articles});
+});
+app.get('/articles/new',(req,res)=>{
+    res.render('new',{title:'Add article'});
 });
 app.listen(5000,()=>{
     console.log('服务启动');

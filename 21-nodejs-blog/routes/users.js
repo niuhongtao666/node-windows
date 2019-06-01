b648ad38864408d84c302670f63966592ed4d96d
niuhongtao <18333602833@163.com>
2019-06-01 12:59:26 +0800

    blog-注册用户表单验证

--- 21-nodejs-blog/routes/users.js
+++ 21-nodejs-blog/routes/users.js
@@ -1,6 +1,58 @@
 let express=require('express');
 let router=express.Router();
+let User=require('../models/user');
+const { check, validationResult } = require('express-validator/check');
 router.get('/register',(req,res)=>{
     res.render('users/register')
+});
+function uniqObjInArray(objarray){
+    let len = objarray.length;
+    let tempJson = {
+        
+    };
+    let res = [];
+    for(let i = 0;i < len;i++){
+        //取出每一个对象
+        tempJson[JSON.stringify(objarray[i])] = true;
+    }
+    console.log("tempJson is ",tempJson);
+    let keyItems= Object.keys(tempJson);
+    for(let j = 0;j < keyItems.length;j++){
+        res.push(JSON.parse(keyItems[j]));
+    }
+    return res;
+}
+router.post('/register',[
+    check('name').isLength({min:1}).withMessage('Name is required'),
+    check('email').isLength({min:1}).withMessage('Email is required'),
+    check('username').isLength({min:1}).withMessage('Username is required'),
+    check('password_confirmation').isLength({min:1}).withMessage('Password-Confirmation is required'),
+    check('email').isEmail().withMessage('Email is invalid'),
+    check('password','password is required'),
+    check('password').isLength({min:1}).withMessage('password is required'),
+    check('password','password is required').trim().custom((value,{req,loc,path})=>{
+            if(value!==req.body.password_confirmation){
+                throw new Error('Passwords do not match');
+            }else {
+                return value;
+            }
+        })
+],(req,res)=>{
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        // return res.status(422).json({ errors: errors.array() });
+        console.log(errors.array());
+        let errorsList=errors.array();
+        errorsList=uniqObjInArray(errorsList);
+        console.dir(errorsList);
+        res.render('users/register',{errors:errorsList,title:'Add article'});
+    }else{
+        let users=new User(req.body);
+        users.save((err,data)=>{
+            if(err) throw err;
+            req.flash("success", "User Add");
+            res.redirect('/');
+        })
+    }
 });
 module.exports=router;
